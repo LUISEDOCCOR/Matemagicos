0176282c8732c25044ca8aa9fa46c1e27f2e808c
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-03-13 11:10:39 -0600

    Recommendations card almost finished, stars need to be added

---
+++ src/data/recommendations.js
@@ -0,0 +1,18 @@
+export const recommendations = [
+    {
+        "name" : "Midudev",
+        "stars": [0,0,0,0],
+    },
+    {
+        "name" : "AforDigital",
+        "stars": [0,0,0,0,0],
+    },
+    {
+        "name" : "Sergio Pérez",
+        "stars": [0,0,0,0],
+    },
+    {
+        "name" : "Carlos Sainz",
+        "stars": [0,0,0,0,0],
+    },
+]
