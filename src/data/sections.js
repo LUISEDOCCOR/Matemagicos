97752c81b6dcdf809aff87d169b0d2d323372977
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-03-13 21:10:56 -0600

    Adding information about topics and images

---
+++ src/data/sections.js
@@ -0,0 +1,32 @@
+export const sections = [
+    {
+        "title": "Criterios de divisibilidad",
+        "content": `Los criterios de divisibilidad son reglas que 
+        nos permiten determinar si un número es
+        divisible por otro sin tener que realizar
+        la división en sí misma.`,
+        "cover_name": "cover-1" 
+    },
+    {
+        "title": "Determina y usa los numeros primos",
+        "content": `Los números primos son números 
+        enteros mayores que 1 que sólo 
+        son divisibles entre sí mismos y 1.`,
+        "cover_name": "cover-2" 
+    },
+    {
+        "title": "Usa los criterios de divisibilidad y los numeros primos",
+        "content": `Los criterios de divisibilidad son 
+        reglas matemáticas que nos ayudan a determinar 
+        si un número es divisible por otro número sin 
+        tener que realizar la división.`,
+        "cover_name": "cover-3" 
+    },
+    {
+        "title": "Minimo Común Múltiplo",
+        "content": `El Mínimo Común Múltiplo (MCM) 
+        es el número más pequeño que es múltiplo común 
+        de dos o más números enteros`,
+        "cover_name": "cover-4" 
+    },
+] 
